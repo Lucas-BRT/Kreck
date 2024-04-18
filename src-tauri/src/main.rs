79ac9c68d66c0daeab3a89b1dfc3275aa9c83ff5
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-04-18 16:13:03 -0300

    FEAT: adds tray basic support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{image::Image, tray::ClickType, Manager};
+
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![])
+        .setup(|app| {
+
+            let tray_icon  = Image::from_bytes(include_bytes!("../icons/icon.ico"))?;
+    
+            // Declared to show tray icon in some Linux distributions;
+            let menu_tray = tauri::menu::MenuBuilder::new(app).build()?;
+    
+            tauri::tray::TrayIconBuilder::new()
+                .icon(tray_icon)
+                .menu(&menu_tray)
+                .build(app)?;
+            Ok(())
+
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
