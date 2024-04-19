deda1365c9d7f026fe6a932c3b9befea30ca14e0
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-04-19 15:09:36 -0300

    FEAT: basic tray controll system now you can close the tray using the close button of the title bar and the application is hidden, but the process keeps working
    needs minnor fixes for multiple systems and some polishment

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{image::Image, menu::{IsMenuItem, MenuBuilder, MenuItemBuilder}, tray::TrayIconBuilder, Manager};
+use tauri::{image::Image, menu::{MenuBuilder, MenuItemBuilder}, tray::TrayIconBuilder, Manager, WindowEvent};
 
 fn main() {
 
@@ -13,8 +13,15 @@ fn main() {
             // Declared to show tray icon in some Linux distributions;
             #[cfg(target_os = "linux")] 
             {
-                let item = MenuItemBuilder::new("Launch Kreck").build(app)?;
-                let menu_tray = MenuBuilder::new(app).item(&item).build()?;
+                let launch_kreck = MenuItemBuilder::new("Launch Kreck")
+                    .build(app)?;
+                let exit_kreck = MenuItemBuilder::new("Exit Kreck")
+                    .build(app)?;
+
+                let menu_tray = MenuBuilder::new(app)
+                    .item(&launch_kreck)
+                    .item(&exit_kreck)
+                    .build()?;
                 
                 TrayIconBuilder::new()
                     .icon(tray_icon)
@@ -22,11 +29,16 @@ fn main() {
                     .build(app)?;
 
                 app.on_menu_event(move |app, event| {
-                    if event.id() == item.id() {
+                    if event.id() == launch_kreck.id() {
                         if let Some(webview_window) = app.get_webview_window("main") {
                             let _ = webview_window.show();
                             let _ = webview_window.set_focus();
                         };
+                    } else if event.id() == exit_kreck.id() {
+                        if let Some(webview_window) = app.get_webview_window("main") {
+                            let _ = webview_window.close();
+                            let _ = webview_window.app_handle().exit(0);
+                        }
                     }
                 })
             }
@@ -39,18 +51,31 @@ fn main() {
                     .icon(tray_icon)
                     .menu(&menu_tray)
                     .on_tray_icon_event(|tray, event| {
-                        
                         if let Some(webview_window) = app.get_webview_window("main") {
                             let _ = webview_window.show();
                             let _ = webview_window.set_focus();
                         }
-
                     })
                     .build(app)?;
             }
 
             Ok(())
-        }) 
+        })
+        .on_window_event(|window, event|  {
+            match event {
+                window_event => {
+                    match window_event {
+                        WindowEvent::CloseRequested { api, .. } => {
+                            api.prevent_close();
+                            if let Some(webview_window) = window.get_webview_window("main") {
+                                webview_window.hide().unwrap();
+                            };
+                        }
+                        _ => ()
+                    }
+                }
+            };
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
