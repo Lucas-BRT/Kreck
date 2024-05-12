be5251be93cc3739b3b07c85de7c304c913d6e24
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-05-12 17:55:21 -0300

    sharing state between threads
    remove unnecessary prints
    
    remove unnecessary fonts
    
    filter user input
    
    remove unecessary arc clone
    
    css rewrite
    
    styles reestructure
    
    filter port input

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,53 +1,80 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{image::Image, 
-            menu::{
-                MenuBuilder, 
-                MenuItemBuilder
-            }, 
-            tray::{
-                TrayIconBuilder,
-                ClickType
-            }, 
-            Manager, 
-            WindowEvent};
-use tauri_plugin_positioner::{WindowExt, Position};
+use kenku_control::{self, Controller};
+use rocket::{fs::FileServer, get, Ignite, Rocket, Shutdown};
+use std::sync::Arc;
+use tauri::{
+    async_runtime::{self, Mutex},
+    image::Image,
+    menu::{MenuBuilder, MenuItemBuilder},
+    tray::TrayIconBuilder,
+    AppHandle, Manager, WindowEvent,
+};
+use tauri_plugin_positioner::{Position, WindowExt};
+
+#[derive(Default)]
+struct RocketShutdownHandle(pub Option<Shutdown>);
 
 #[tauri::command]
-async fn launch_server() {
+async fn request_server_shutdown(app_state: AppHandle) {
+    let state = app_state.state::<Arc<Mutex<RocketShutdownHandle>>>();
+    let handle = state.try_lock().unwrap();
 
+    if let Some(handler) = &handle.0 {
+        handler.clone().notify();
+    }
 }
 
 #[tauri::command]
-fn hello_world() {
-    println!("hello world");
+async fn setup_server() -> Result<Rocket<Ignite>, rocket::Error> {
+    let rocket_config = rocket::Config::figment()
+        .merge(("address", "0.0.0.0"))
+        .merge(("port", 8000))
+        .merge(("workers", 4));
+
+    rocket::custom(rocket_config)
+        .mount("/", FileServer::from("../src"))
+        .ignite()
+        .await
+}
+
+#[tauri::command]
+async fn launch_server(app_state: AppHandle) {
+    let state = app_state.state::<Arc<Mutex<RocketShutdownHandle>>>();
+    let mut handle = state.try_lock().unwrap();
+    let server = setup_server().await.unwrap();
+    let shutdown_handle = server.shutdown();
+
+    handle.0 = Some(shutdown_handle);
+
+    async_runtime::spawn(async move {
+        server.launch().await.unwrap();
+    });
 }
 
 fn main() {
-
     tauri::Builder::default()
         .plugin(tauri_plugin_positioner::init())
-        .setup(|app|{
-            
-            let launch_kreck = MenuItemBuilder::new("Launch Kreck")
-                    .build(app)?;
-            let exit_kreck = MenuItemBuilder::new("Exit Kreck")
-                    .build(app)?;
+        .setup(|app| {
+            let shutdown_handle = Arc::new(Mutex::new(RocketShutdownHandle(None)));
 
-            let tray_icon = Image::from_bytes(&include_bytes!("../icons/icon.ico").to_vec()).expect("failed to create icon from image ../icons/icon.ico");
+            app.manage(shutdown_handle);
 
+            let launch_kreck = MenuItemBuilder::new("Launch Kreck").build(app)?;
+            let exit_kreck = MenuItemBuilder::new("Exit Kreck").build(app)?;
+
+            let tray_icon = Image::from_bytes(&include_bytes!("../icons/icon.ico").to_vec())
+                .expect("failed to create icon from image ../icons/icon.ico");
 
             #[cfg(target_os = "linux")]
             let menu_tray = MenuBuilder::new(app)
-                    .item(&launch_kreck)
-                    .item(&exit_kreck)
-                    .build()?;
+                .item(&launch_kreck)
+                .item(&exit_kreck)
+                .build()?;
 
             #[cfg(not(target_os = "linux"))]
-            let menu_tray = MenuBuilder::new(app)
-                    .item(&exit_kreck)
-                    .build()?;
+            let menu_tray = MenuBuilder::new(app).item(&exit_kreck).build()?;
 
             TrayIconBuilder::new()
                 .icon(tray_icon)
@@ -57,20 +84,31 @@ fn main() {
                     {
                         match event.click_type {
                             ClickType::Left => {
-                                if let Some(webview_window) = app.app_handle().get_webview_window("main") {
-                                    tauri_plugin_positioner::on_tray_event(app.app_handle(), &event);
+                                if let Some(webview_window) =
+                                    app.app_handle().get_webview_window("main")
+                                {
+                                    tauri_plugin_positioner::on_tray_event(
+                                        app.app_handle(),
+                                        &event,
+                                    );
                                     let _ = webview_window.show();
-                                    let _ = webview_window.as_ref().window().move_window(Position::TrayCenter);       
+                                    let _ = webview_window
+                                        .as_ref()
+                                        .window()
+                                        .move_window(Position::TrayCenter);
                                 }
                             }
-                            _ => ()
+                            _ => (),
                         }
                     }
-               })
+                })
                 .on_menu_event(move |app, event| {
                     if event.id() == launch_kreck.id() {
                         if let Some(webview_window) = app.get_webview_window("main") {
-                            let win = webview_window.app_handle().get_webview_window("main").unwrap();
+                            let win = webview_window
+                                .app_handle()
+                                .get_webview_window("main")
+                                .unwrap();
                             let _ = win.as_ref().window().move_window(Position::Center);
                             let _ = webview_window.show();
                             let _ = webview_window.set_focus();
@@ -83,10 +121,10 @@ fn main() {
                     }
                 })
                 .build(app)?;
-           
+
             Ok(())
         })
-        .on_window_event(|window, event|  {
+        .on_window_event(|window, event| {
             match event {
                 window_event => {
                     match window_event {
@@ -95,18 +133,21 @@ fn main() {
                             if let Some(webview_window) = window.get_webview_window("main") {
                                 webview_window.hide().unwrap();
                             };
-                        },
+                        }
                         // WindowEvent::Focused(false) => {
                         //     if let Some(webview_window) = window.get_webview_window("main") {
                         //         webview_window.hide().unwrap();
                         //     };
                         // },
-                        _ => ()
+                        _ => (),
                     }
                 }
             };
         })
-        .invoke_handler(tauri::generate_handler![hello_world])
+        .invoke_handler(tauri::generate_handler![
+            launch_server,
+            request_server_shutdown
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/assets/scripts/main.js
@@ -0,0 +1,7 @@
+import { handleConnectButton } from './button.js';
+import { handleIpInput, handlePortInput } from './handleInput.js';
+
+handleConnectButton("#connect-button")
+handleIpInput(".ip-entry")
+handlePortInput("#port")
+
