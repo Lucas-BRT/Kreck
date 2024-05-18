27332509f064d399a8efe527c6c506d8de727143
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-05-18 15:00:10 -0300

    feat: allow mobile front-end to request kenku_control methods

--- src-tauri/src/rocket_endpoints.rs
+++ src-tauri/src/rocket_endpoints.rs
@@ -1,5 +1,5 @@
 use kenku_control::*;
-use rocket::{fs::FileServer, get, routes, Ignite, Rocket, Shutdown, State};
+use rocket::{fs::FileServer, get, put, routes, Ignite, Rocket, Shutdown, State};
 use serde_json::{json, Value};
 
 #[derive(Default)]
@@ -9,14 +9,14 @@ pub struct RocketShutdownHandle(pub Option<Shutdown>);
 pub async fn get_tracks(controller: &State<Controller>) -> Value {
     let tracks = controller.get_playlist().await.unwrap();
 
-    json!(tracks.tracks)
+    json!(tracks)
 }
 
 #[get("/sounds")]
 pub async fn get_sounds(controller: &State<Controller>) -> Value {
     let sounds = controller.get_soundboard().await.unwrap();
 
-    json!(sounds.sounds)
+    json!(sounds)
 }
 
 #[get("/playlist-playback")]
@@ -33,6 +33,45 @@ pub async fn get_soundboard_playback(controller: &State<Controller>) -> Value {
     json!(soundboard)
 }
 
+#[put("/play/<track_id>")]
+pub async fn play_track(controller: &State<Controller>, track_id: String) {
+    let tracks = controller.get_playlist().await.unwrap().tracks;
+
+    for track in tracks {
+        if track.id == track_id {
+            track.play(&controller).await.unwrap();
+        }
+    }
+}
+
+#[put("/playlist-playback-pause")]
+pub async fn pause_playback(controller: &State<Controller>) {
+    playlist::playback::playback_pause(&controller)
+        .await
+        .unwrap();
+}
+
+#[put("/playlist-playback-play")]
+pub async fn play_playback(controller: &State<Controller>) {
+    playlist::playback::playback_play(&controller)
+        .await
+        .unwrap();
+}
+
+#[put("/playlist-playback-next")]
+pub async fn next_playback(controller: &State<Controller>) {
+    playlist::playback::playback_next(&controller)
+        .await
+        .unwrap();
+}
+
+#[put("/playlist-playback-previous")]
+pub async fn previous_playback(controller: &State<Controller>) {
+    playlist::playback::playback_previous(&controller)
+        .await
+        .unwrap();
+}
+
 pub async fn setup_server(controller: Controller) -> Result<Rocket<Ignite>, rocket::Error> {
     let rocket_config = rocket::Config::figment()
         .merge(("address", "0.0.0.0"))
@@ -42,7 +81,20 @@ pub async fn setup_server(controller: Controller) -> Result<Rocket<Ignite>, rock
     rocket::custom(rocket_config)
         .manage(controller)
         .mount("/", FileServer::from("../src-mobile"))
-        .mount("/", routes![get_tracks, get_sounds, get_playlist_playback, get_soundboard_playback])
+        .mount(
+            "/",
+            routes![
+                get_tracks,
+                get_sounds,
+                get_playlist_playback,
+                get_soundboard_playback,
+                play_track,
+                pause_playback,
+                play_playback,
+                next_playback,
+                previous_playback
+            ],
+        )
         .ignite()
         .await
 }
