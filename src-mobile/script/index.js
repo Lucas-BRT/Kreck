1935e54615429a2196cec80ccd86c765e8226cc6
Leonardo Rühmann <leonardoruhmannwork@gmail.com>
2024-05-27 20:05:40 -0300

    feat: add extensive features to Kreck

--- src-mobile/script/index.js
+++ src-mobile/script/index.js
@@ -1,7 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".play-button").addEventListener("click", () => {
-    let playbackbar = document.querySelector(".playbackbar");
-    playbackbar.style.display = "flex";
-    console.log("Div");
-  });
-});
+import { renderTracksList } from "./components/renderTracks.js";
+import { getTracks } from "./endpoints.js";
+import { handlePlayButton } from "./handlers/handlePlayButton.js";
+import { convertTracks } from "./handlers/handleTracks.js";
+import { handlePlayPlaybackbar } from "./handlers/handlePlayPlaybackbar.js";
+import { handleNextPlaybackbar } from "./handlers/handleNextPlaybackbar.js";
+import { handlePreviousPlaybackbar } from "./handlers/handlePreviousPlaybackbar.js";
+import { checkText } from "./components/checkTextAndAddCarroussel.js";
+
+const tracks = await getTracks();
+const convertedTracks = convertTracks(tracks);
+
+renderTracksList(convertedTracks);
+handlePlayButton(convertedTracks);
+handlePlayPlaybackbar();
+handleNextPlaybackbar();
+handlePreviousPlaybackbar();
+checkText("track-name");
