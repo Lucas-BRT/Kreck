6db1ada04f15f2221893fb168a2dbf55b9948f9c
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-05-29 15:14:24 -0300

    check if kenku is avaliable before creating the controller

--- src-tauri/src/kenku_remote_controller.rs
+++ src-tauri/src/kenku_remote_controller.rs
@@ -1,6 +1,11 @@
 use kenku_control::Controller;
+use std::{net::{Ipv4Addr, SocketAddrV4}, str::FromStr};
 
-pub fn setup_kenku_controller(ip: String, port: u16) -> Option<Controller> {
-    let controller = Controller::new(ip, port);
-    Some(controller)
+pub async fn setup_kenku_controller(ip: String, port: u16) -> Result<Controller, String> {
+    let address = SocketAddrV4::new(Ipv4Addr::from_str(ip.as_str()).expect("invalid Ip address"), port);
+
+    match kenku_control::utils::check_kenku_server_state(address).await {
+        kenku_control::KenkuState::Online => Ok(Controller::new(ip, port)),
+        kenku_control::KenkuState::Offline => Err("Server Offline".to_string())
+    }
 }
