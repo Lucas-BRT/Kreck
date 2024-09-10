61b9760352a89ceb0e363d813b34be5a60af40be
lucasBRT <lucasbritodepaula.t@gmail.com>
2024-09-10 16:40:34 -0300

    refactor: fix some warnings

--- src-tauri/src/server/endpoints.rs
+++ src-tauri/src/server/endpoints.rs
@@ -1,12 +1,12 @@
+use kenku_control::{playlist::playback::*, Controller};
 use rocket::{get, put, State};
 use serde_json::{json, Value};
-use kenku_control::{Controller, playlist::playback::*};
 
 #[get("/tracks")]
 pub async fn get_tracks(controller: &State<Controller>) -> Result<Value, ()> {
     match controller.get_playlist().await {
         Ok(tracks) => Ok(json!(tracks)),
-        Err(e) => Err(println!("{e}"))
+        Err(e) => Err(println!("{e}")),
     }
 }
 
@@ -14,7 +14,7 @@ pub async fn get_tracks(controller: &State<Controller>) -> Result<Value, ()> {
 pub async fn get_sounds(controller: &State<Controller>) -> Result<Value, ()> {
     match controller.get_soundboard().await {
         Ok(sounds) => Ok(json!(sounds)),
-        Err(e) => Err(println!("{e}"))
+        Err(e) => Err(println!("{e}")),
     }
 }
 
@@ -22,7 +22,7 @@ pub async fn get_sounds(controller: &State<Controller>) -> Result<Value, ()> {
 pub async fn get_playlist_playback(controller: &State<Controller>) -> Result<Value, ()> {
     match controller.get_playlist_playback().await {
         Ok(playback) => Ok(json!(playback)),
-        Err(e) => Err(println!("{e}"))
+        Err(e) => Err(println!("{e}")),
     }
 }
 
@@ -30,7 +30,7 @@ pub async fn get_playlist_playback(controller: &State<Controller>) -> Result<Val
 pub async fn get_soundboard_playback(controller: &State<Controller>) -> Result<Value, ()> {
     match controller.get_soundboard_playback().await {
         Ok(soundboard) => Ok(json!(soundboard)),
-        Err(e) => Err(println!("{e}"))
+        Err(e) => Err(println!("{e}")),
     }
 }
 
@@ -40,43 +40,42 @@ pub async fn play_track(controller: &State<Controller>, track_id: String) {
         Ok(tracks) => {
             for track in tracks.tracks {
                 if track.id == track_id {
-                    track.play(&controller).await.unwrap();
+                    track.play(controller).await.unwrap();
                 }
             }
         }
-        Err(e) => println!("{e}")
+        Err(e) => println!("{e}"),
     };
 }
 
 #[put("/playlist-playback-pause")]
 pub async fn pause_playback(controller: &State<Controller>) {
-    match playback_pause(&controller).await {
+    match playback_pause(controller).await {
         Ok(_) => println!("PAUSE PLAYBACK"),
-        Err(e) => println!("{e}")
+        Err(e) => println!("{e}"),
     }
 }
 
 #[put("/playlist-playback-play")]
 pub async fn play_playback(controller: &State<Controller>) {
-    match playback_play(&controller).await {
+    match playback_play(controller).await {
         Ok(_) => println!("PLAY PLAYBACK"),
-        Err(e) => println!("{e}")
+        Err(e) => println!("{e}"),
     }
 }
 
 #[put("/playlist-playback-next")]
 pub async fn next_playback(controller: &State<Controller>) {
-    match playback_next(&controller).await {
+    match playback_next(controller).await {
         Ok(_) => println!("NEXT PLAYBACK"),
-        Err(e) => println!("{e}")
+        Err(e) => println!("{e}"),
     }
 }
 
 #[put("/playlist-playback-previous")]
 pub async fn previous_playback(controller: &State<Controller>) {
-    match playback_previous(&controller).await {
+    match playback_previous(controller).await {
         Ok(_) => println!("PREVIOUS PLAYBACK"),
-        Err(e) => println!("{e}")
+        Err(e) => println!("{e}"),
     }
 }
-
