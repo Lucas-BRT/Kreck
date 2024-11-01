cf1af8a68d79bef19c2378425621a402243b97a4
alanzin1 <alanzinvictorapt@gmail.com>
2024-10-31 23:54:15 -0300

    feat: add sound conversion and rendering logic to the controller

--- src-mobile/script/sounds.js
+++ src-mobile/script/sounds.js
@@ -1,5 +1,8 @@
 import { getSounds } from "./endpoints.js";
 import { convertSounds } from "./handlers/handleSounds.js";
+import { renderSoundsList } from "./components/renderSounds.js";
 
 const sounds = await getSounds();
 const convertedSounds = convertSounds(sounds);
+
+renderSoundsList(convertedSounds);
