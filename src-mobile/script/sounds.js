1883e3e4c74336a75e5d685a2a5ddc5e22485359
alanzin1 <alanzinvictorapt@gmail.com>
2024-10-31 23:34:54 -0300

    feat: add sound get and conversion logic to the controller

--- src-mobile/script/sounds.js
+++ src-mobile/script/sounds.js
@@ -1,1 +1,5 @@
 import { getSounds } from "./endpoints.js";
+import { convertSounds } from "./handlers/handleSounds.js";
+
+const sounds = await getSounds();
+const convertedSounds = convertSounds(sounds);
